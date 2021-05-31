f1772ed47422fe97b2f8d5a9ccb4e991cbafadd5
“yourname” <82037182@qq.com>
2021-06-01 00:30:24 +0800

    第9课 - 主引导程序控制权的转移

--- code/boot.asm
+++ code/boot.asm
@@ -72,11 +72,24 @@ start:
 	
 	call ReadSector
 	
-	mov cx, [EntryItem + 0x1A]
+	mov dx, [EntryItem + 0x1A]
+	mov si, BaseOfLoader
 	
-	call FatVec
-	
-	jmp last
+loading:
+    mov ax, dx
+    add ax, 31
+    mov cx, 1
+    push dx
+    push bx
+    mov bx, si
+    call ReadSector
+    pop bx
+    pop cx
+    call FatVec
+    cmp dx, 0xFF7
+    jnb BaseOfLoader
+    add si, 512
+    jmp loading
 	
 output:	
     mov bp, MsgStr
@@ -148,10 +161,6 @@ return:
 ; es:di --> destination
 ; cx    --> length
 MemCpy:
-    push si
-    push di
-    push cx
-    push ax
     
     cmp si, di
     
@@ -185,10 +194,6 @@ etob:
     jmp etob
 
 done:   
-    pop ax
-    pop cx
-    pop di
-    pop si
     ret
 
 ; es:bx --> root entry offset address
@@ -199,8 +204,6 @@ done:
 ;     (dx !=0 ) ? exist : noexist
 ;        exist --> bx is the target entry
 FindEntry:
-    push di
-    push bp
     push cx
     
     mov dx, [BPB_RootEntCnt]
@@ -211,7 +214,9 @@ find:
     jz noexist
     mov di, bx
     mov cx, [bp]
+    push si
     call MemCmp
+    pop si
     cmp cx, 0
     jz exist
     add bx, 32
@@ -221,8 +226,6 @@ find:
 exist:
 noexist: 
     pop cx
-    pop bp
-    pop di
        
     ret
 
@@ -233,10 +236,7 @@ noexist:
 ; return:
 ;        (cx == 0) ? equal : noequal
 MemCmp:
-    push si
-    push di
-    push ax
-    
+
 compare:
     cmp cx, 0
     jz equal
@@ -252,10 +252,7 @@ goon:
     
 equal: 
 noequal:   
-    pop ax
-    pop di
-    pop si
-    
+
     ret
 
 ; es:bp --> string address
@@ -269,15 +266,10 @@ Print:
 
 ; no parameter
 ResetFloppy:
-    push ax
-    push dx
     
     mov ah, 0x00
     mov dl, [BS_DrvNum]
     int 0x13
-    
-    pop dx
-    pop ax
     
     ret
 
@@ -285,10 +277,6 @@ ResetFloppy:
 ; cx    --> number of sector
 ; es:bx --> target address
 ReadSector:
-    push bx
-    push cx
-    push dx
-    push ax
     
     call ResetFloppy
     
@@ -313,11 +301,6 @@ ReadSector:
 read:    
     int 0x13
     jc read
-    
-    pop ax
-    pop dx
-    pop cx
-    pop bx
     
     ret
 
---
+++ code/loader.asm
@@ -0,0 +1,24 @@
+org 0x9000
+
+begin:
+    mov si, msg
+
+print:
+    mov al, [si]
+    add si, 1
+    cmp al, 0x00
+    je end
+    mov ah, 0x0E
+    mov bx, 0x0F
+    int 0x10
+    jmp print
+
+end:
+    hlt
+    jmp end
+    
+msg:
+    db 0x0a, 0x0a
+    db "Hello, D.T.OS!"
+    db 0x0a, 0x0a
+    db 0x00
