3bda3b4e2dfef9e8fd384a8ab9cf6eb6a31c81f0
“yourname” <82037182@qq.com>
2021-06-01 01:22:49 +0800

    第62课 - 进程互斥锁的初步实现（上）

---
+++ code/D.T.OS/ihandler.c
@@ -0,0 +1,55 @@
+
+#include "interrupt.h"
+#include "task.h"
+#include "mutex.h"
+
+extern volatile Task* gCTaskAddr;
+
+void TimerHandler()
+{
+    static uint i = 0;
+    
+    i = (i + 1) % 5;
+    
+    if( i == 0 )
+    {
+        Schedule();
+    }
+    
+    SendEOI(MASTER_EOI_PORT);
+}
+
+void SysCallHandler(uint type, uint cmd, uint param1, uint param2)   // __cdecl__
+{  
+    switch(type)
+    {
+        case 0:
+            KillTask();
+            break;
+        case 1:
+            MutexCallHandler(cmd, param1);
+            break;
+        default:
+            break;
+    }
+}
+
+void PageFaultHandler()
+{
+    SetPrintPos(0, 6);
+    
+    PrintString("Page Fault: kill ");
+    PrintString(gCTaskAddr->name);
+    
+    KillTask();
+}
+
+void SegmentFaultHandler()
+{
+    SetPrintPos(0, 6);
+    
+    PrintString("Segment Fault: kill ");
+    PrintString(gCTaskAddr->name);
+    
+    KillTask();
+}
