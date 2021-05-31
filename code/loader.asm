191745e7691810a632839a8e71a1fdb303c01c75
“yourname” <82037182@qq.com>
2021-06-01 00:45:36 +0800

    第29课 - 畅游 x86 分页机制（中）

--- code/loader.asm
+++ code/loader.asm
@@ -5,22 +5,27 @@ PageTblBase0    equ    0x201000
 PageDirBase1    equ    0x300000
 PageTblBase1    equ    0x301000
 
+ObjectAddrX     equ    0x401000
+TargetAddrY     equ    0x501000
+TargetAddrZ     equ    0x601000
+
 org 0x9000
 
 jmp ENTRY_SEGMENT
 
 [section .gdt]
 ; GDT definition
-;                                     段基址，           段界限，       段属性
-GDT_ENTRY       :     Descriptor        0,                0,           0
-CODE32_DESC     :     Descriptor        0,        Code32SegLen - 1,    DA_C + DA_32
-VIDEO_DESC      :     Descriptor     0xB8000,         0x07FFF,         DA_DRWA + DA_32
-DATA32_DESC     :     Descriptor        0,        Data32SegLen - 1,    DA_DRW + DA_32
-STACK32_DESC    :     Descriptor        0,         TopOfStack32,       DA_DRW + DA_32
-PAGE_DIR_DESC0  :     Descriptor    PageDirBase0,       4095,          DA_DRW + DA_32
-PAGE_TBL_DESC0  :     Descriptor    PageTblBase0,       1023,          DA_DRW + DA_LIMIT_4K + DA_32
-PAGE_DIR_DESC1  :     Descriptor    PageDirBase1,       4095,          DA_DRW + DA_32
-PAGE_TBL_DESC1  :     Descriptor    PageTblBase1,       1023,          DA_DRW + DA_LIMIT_4K + DA_32
+;                                       段基址，           段界限，       段属性
+GDT_ENTRY         :     Descriptor        0,                0,           0
+CODE32_DESC       :     Descriptor        0,        Code32SegLen - 1,    DA_C + DA_32
+VIDEO_DESC        :     Descriptor     0xB8000,         0x07FFF,         DA_DRWA + DA_32
+DATA32_DESC       :     Descriptor        0,        Data32SegLen - 1,    DA_DRW + DA_32
+STACK32_DESC      :     Descriptor        0,         TopOfStack32,       DA_DRW + DA_32
+PAGE_DIR_DESC0    :     Descriptor    PageDirBase0,       4095,          DA_DRW + DA_32
+PAGE_TBL_DESC0    :     Descriptor    PageTblBase0,       1023,          DA_DRW + DA_LIMIT_4K + DA_32
+PAGE_DIR_DESC1    :     Descriptor    PageDirBase1,       4095,          DA_DRW + DA_32
+PAGE_TBL_DESC1    :     Descriptor    PageTblBase1,       1023,          DA_DRW + DA_LIMIT_4K + DA_32
+FLAT_MODE_RW_DESC :     Descriptor        0,             0xFFFFF,        DA_DRW + DA_LIMIT_4K + DA_32
 ; GDT end
 
 GdtLen    equ   $ - GDT_ENTRY
@@ -32,14 +37,15 @@ GdtPtr:
           
 ; GDT Selector
 
-Code32Selector   equ (0x0001 << 3) + SA_TIG + SA_RPL0
-VideoSelector    equ (0x0002 << 3) + SA_TIG + SA_RPL0
-Data32Selector   equ (0x0003 << 3) + SA_TIG + SA_RPL0
-Stack32Selector  equ (0x0004 << 3) + SA_TIG + SA_RPL0
-PageDirSelector0 equ (0x0005 << 3) + SA_TIG + SA_RPL0
-PageTblSelector0 equ (0x0006 << 3) + SA_TIG + SA_RPL0
-PageDirSelector1 equ (0x0007 << 3) + SA_TIG + SA_RPL0
-PageTblSelector1 equ (0x0008 << 3) + SA_TIG + SA_RPL0
+Code32Selector      equ (0x0001 << 3) + SA_TIG + SA_RPL0
+VideoSelector       equ (0x0002 << 3) + SA_TIG + SA_RPL0
+Data32Selector      equ (0x0003 << 3) + SA_TIG + SA_RPL0
+Stack32Selector     equ (0x0004 << 3) + SA_TIG + SA_RPL0
+PageDirSelector0    equ (0x0005 << 3) + SA_TIG + SA_RPL0
+PageTblSelector0    equ (0x0006 << 3) + SA_TIG + SA_RPL0
+PageDirSelector1    equ (0x0007 << 3) + SA_TIG + SA_RPL0
+PageTblSelector1    equ (0x0008 << 3) + SA_TIG + SA_RPL0
+FlatModeRWSelector  equ (0x0009 << 3) + SA_TIG + SA_RPL0
 ; end of [section .gdt]
 
 TopOfStack16    equ 0x7c00
@@ -48,8 +54,10 @@ TopOfStack16    equ 0x7c00
 [bits 32]
 DATA32_SEGMENT:
     DTOS               db  "D.T.OS!", 0
+    DTOS_LEN           equ $ - DTOS
     DTOS_OFFSET        equ DTOS - $$
     HELLO_WORLD        db  "Hello World!", 0
+    HELLO_WORLD_LEN    equ $ - HELLO_WORLD
     HELLO_WORLD_OFFSET equ HELLO_WORLD - $$
 
 Data32SegLen equ $ - DATA32_SEGMENT
@@ -140,19 +148,20 @@ CODE32_SEGMENT:
     mov ax, Data32Selector
     mov ds, ax
     
-    mov ebp, DTOS_OFFSET
-    mov bx, 0x0C
-    mov dh, 12
-    mov dl, 33
+    mov ax, FlatModeRWSelector
+    mov es, ax
     
-    call PrintString
+    mov esi, DTOS_OFFSET
+    mov edi, TargetAddrY
+    mov ecx, DTOS_LEN
     
-    mov ebp, HELLO_WORLD_OFFSET
-    mov bx, 0x0C
-    mov dh, 13
-    mov dl, 31
+    call MemCpy32
     
-    call PrintString
+    mov esi, HELLO_WORLD_OFFSET
+    mov edi, TargetAddrZ
+    mov ecx, HELLO_WORLD_LEN
+    
+    call MemCpy32
     
     mov eax, PageDirSelector0
     mov ebx, PageTblSelector0
@@ -166,16 +175,120 @@ CODE32_SEGMENT:
     
     call InitPageTable
     
-    mov eax, PageDirBase0
+    mov eax, ObjectAddrX   ; 0x401000
+    mov ebx, TargetAddrY   ; 0x501000
+    mov ecx, PageDirBase0
     
-    call SwitchPageTable
+    call MapAddress
     
-    mov eax, PageDirBase1
+    mov eax, ObjectAddrX   ; 0x401000
+    mov ebx, TargetAddrZ   ; 0x601000
+    mov ecx, PageDirBase1
     
-    call SwitchPageTable
+    call MapAddress
+    
+    ; mov eax, PageDirBase0
+    
+    ; call SwitchPageTable
+    
+    ; mov eax, PageDirBase1
+    
+    ; call SwitchPageTable
     
     jmp $
 
+; es  --> flat mode
+; eax --> virtual address
+; ebx --> target  address
+; ecx --> page directory base
+MapAddress:
+    push edi
+    push esi 
+    push eax    ; [esp + 8]
+    push ebx    ; [esp + 4]
+    push ecx    ; [esp]
+    
+    ; 1. 取虚地址高 10 位， 计算子页表在页目录中的位置
+    mov eax, [esp + 8]
+    shr eax, 22
+    and eax, 1111111111b
+    shl eax, 2
+    
+    ; 2. 取虚地址中间 10 位， 计算物理地址在子页表中的位置
+    mov ebx, [esp + 8]
+    shr ebx, 12
+    and ebx, 1111111111b
+    shl ebx, 2
+    
+    ; 3. 取子页表起始地址
+    mov esi, [esp]
+    add esi, eax
+    mov edi, [es:esi]
+    and edi, 0xFFFFF000
+    
+    ; 4. 将目标地址写入子页表的对应位置
+    add edi, ebx
+    mov ecx, [esp + 4]
+    and ecx, 0xFFFFF000
+    or  ecx, PG_P | PG_USU | PG_RWW
+    mov [es:edi], ecx
+    
+    pop ecx
+    pop ebx
+    pop eax
+    pop esi
+    pop edi
+    
+    ret
+
+; es     --> flat mode selector
+; ds:esi --> source
+; es:edi --> destination
+; ecx    --> length
+MemCpy32:
+    push esi
+    push edi
+    push ecx
+    push ax
+    
+    cmp esi, edi
+    
+    ja btoe
+    
+    add esi, ecx
+    add edi, ecx
+    dec esi
+    dec edi
+    
+    jmp etob
+    
+btoe:
+    cmp ecx, 0
+    jz done
+    mov al, [ds:esi]
+    mov byte [es:edi], al
+    inc esi
+    inc edi
+    dec ecx
+    jmp btoe
+    
+etob: 
+    cmp ecx, 0
+    jz done
+    mov al, [ds:esi]
+    mov byte [es:edi], al
+    dec esi
+    dec edi
+    dec ecx
+    jmp etob
+
+done:   
+    pop ax
+    pop ecx
+    pop edi
+    pop esi
+    ret
+    
 ; eax --> page dir base selector
 ; ebx --> page table base selector 
 ; ecx --> page table base
